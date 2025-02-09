32a028720cfdb84eef8408a1d817127de59998f9
nocapusd <zonnix336@gmail.com>
2025-02-09 13:40:31 +0300

    lesson 1 done

---
+++ lesson 1/assets/js/main.js
@@ -0,0 +1,52 @@
+// lesson 1.1
+
+let greeting = "hello";
+let domainName = "owu";
+let domain = "com";
+let domainCountry = "ua";
+let numberFirst = 1;
+let numberOfFingers = 10;
+let negativeNumber = -999;
+let randomNumber = 123;
+let pi = 3.14;
+let randomFloat = 2.7;
+let ageOfRandomGuy = 16;
+let codeWork = true;
+let codeDoesntWork = false;
+
+console.log(greeting);
+console.log(domainName);
+console.log(domain);
+console.log(domainCountry);
+console.log(numberFirst);
+console.log(numberOfFingers);
+console.log(negativeNumber);
+console.log(randomNumber);
+console.log(pi);
+console.log(randomFloat);
+console.log(ageOfRandomGuy);
+console.log(codeWork);
+console.log(codeDoesntWork);
+
+
+
+// lesson 1.2
+
+let lastName = "Kruhliakov";
+let firstName = "Nazar";
+let middleName = "Oleksandrovich";
+
+let person = `${lastName} ${firstName} ${middleName}`
+
+console.log(person)
+
+// lesson 1.3
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
---
+++ lesson 1/prompt/main.js
@@ -0,0 +1,11 @@
+// prompt()
+
+let userName = prompt("Як тебе звати?")
+let userMiddleName = prompt("По батькові?")
+let userAge = prompt("Скільки тобі років?");
+
+let userResult = `${userName} ${userMiddleName}`
+let userAgeResult = `${userAge}`
+
+console.log("Привіт " + userResult + "!")
+console.log(userAgeResult + " Гарний вік!")
